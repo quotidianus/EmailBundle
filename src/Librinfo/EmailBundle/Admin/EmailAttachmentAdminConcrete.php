ddbfc4fbb88b9bdd4ec57361bae09809b042221d
Romain Sanchez <romainsancho@gmail.com>
2016-04-07 17:52:26 +0200

    upload button visible

--- src/Librinfo/EmailBundle/Admin/EmailAttachmentAdminConcrete.php
+++ src/Librinfo/EmailBundle/Admin/EmailAttachmentAdminConcrete.php
@@ -7,4 +7,21 @@ use Librinfo\CoreBundle\Admin\Traits\EmbeddedAdmin;
 class EmailAttachmentAdminConcrete extends EmailAttachmentAdmin
 {
     use EmbeddedAdmin;
+
+    public function prePersist($attachement)
+    {
+        $this->manageFileUpload($attachment);
+    }
+
+    public function preUpdate($attachment)
+    {
+        $this->manageFileUpload($attachment);
+    }
+
+    private function manageFileUpload($attachment)
+    {
+        if ($attachment->getFile()) {
+            $attachment->refreshUpdated();
+        }
+    }
 }
--- src/Librinfo/EmailBundle/Entity/EmailAttachment.php
+++ src/Librinfo/EmailBundle/Entity/EmailAttachment.php
@@ -2,20 +2,53 @@
 
 namespace Librinfo\EmailBundle\Entity;
 
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
 /**
  * EmailAttachment
  */
 class EmailAttachment extends File
 {
+    const ATTACHMENTS_DIR = __DIR__;
+
     /**
      * @var \Librinfo\EmailBundle\Entity\Email
      */
     private $email;
 
-    // /**
-    // *@var File file
-    // */
-    // private $file;
+    /**
+    *@var UploadedFile file
+    */
+    private $file;
+
+    /**
+    *@var DateTime updated
+    */
+    private $updated;
+
+    /**
+     * Set email
+     *
+     * @param DateTime updated
+     *
+     * @return updated
+     */
+    public function setUpdated($updated)
+    {
+        $this->updated = $updated;
+
+        return $this;
+    }
+
+    /**
+     * Get updated
+     *
+     * @return DateTime updated
+     */
+    public function getUpdated()
+    {
+        return $this->updated;
+    }
 
     /**
      * Set email
@@ -41,15 +74,57 @@ class EmailAttachment extends File
         return $this->email;
     }
 
-    // public function setFile(File $file)
-    // {
-    //     $this->file = $file;
-    //
-    //     return $this;
-    // }
-    //
-    // public function getFile()
-    // {
-    //     return $this->file;
-    // }
+    public function setFile(UploadedFile $file = null)
+    {
+        $this->file = $file;
+
+        return $this;
+    }
+
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    /**
+ * Manages the copying of the file to the relevant place on the server
+ */
+    public function upload()
+    {
+        // the file property can be empty if the field is not required
+        if (null === $this->getFile()) {
+            return;
+        }
+
+        // we use the original file name here but you should
+        // sanitize it at least to avoid any security issues
+
+        // move takes the target directory and target filename as params
+        $this->getFile()->move(
+            self::ATTACHMENTS_DIR,
+            $this->getFile()->getClientOriginalName()
+        );
+
+        // set the path property to the filename where you've saved the file
+        $this->filename = $this->getFile()->getClientOriginalName();
+
+        // clean up the file property as you won't need it anymore
+        $this->setFile(null);
+    }
+
+    /**
+     * Lifecycle callback to upload the file to the server
+     */
+    public function lifecycleFileUpload()
+    {
+        $this->upload();
+    }
+
+    /**
+     * Updates the hash value to force the preUpdate and postUpdate events to fire
+     */
+    public function refreshUpdated()
+    {
+        $this->setUpdated(new \DateTime());
+    }
 }

45dc9ff2c82f7466fedd2926035706b3376083bf
Romain Sanchez <romainsancho@gmail.com>
2016-04-08 17:56:32 +0200

    attachment upload working(hardcoded uploadDir)

--- src/Librinfo/EmailBundle/DependencyInjection/Configuration.php
+++ src/Librinfo/EmailBundle/DependencyInjection/Configuration.php
@@ -20,9 +20,13 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('librinfo_email');
 
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
+        $rootNode
+            ->children()
+                ->scalarNode('upload_path')
+                ->end()
+            ->end()
+        ;
+
 
         return $treeBuilder;
     }

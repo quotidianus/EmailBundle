84f4759e64f32e9b1305b5f1b8a1a1ac77c2d3ee
François GERBES <francois.gerbes@libre-informatique.fr>
2017-05-03 18:29:41 +0200

    Rollback to standard version

--- bin/ci-scripts/install_test.sh
+++ bin/ci-scripts/install_test.sh
@@ -16,14 +16,8 @@ chmod u+x "${HOME}/bin/phpunit"
 wget https://github.com/satooshi/php-coveralls/releases/download/v1.0.1/coveralls.phar --output-document="${HOME}/bin/coveralls"
 chmod u+x "${HOME}/bin/coveralls"
 
-
-#composer clear-cache --no-interaction
-composer install --prefer-dist --no-interaction --prefer-stable 
-
-
 # To be removed when this issue will be resolved: https://github.com/composer/composer/issues/5355
 if [ "${COMPOSER_FLAGS}" = '--prefer-lowest' ]; then
     composer update --prefer-dist --no-interaction --prefer-stable --quiet
 fi
-
 composer update --prefer-dist --no-interaction --prefer-stable ${COMPOSER_FLAGS}

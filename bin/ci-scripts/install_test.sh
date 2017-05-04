df0317db7a6e776534c35dd2c2ba0efc92fb9b8e
François GERBES <francois.gerbes@libre-informatique.fr>
2017-05-04 18:39:09 +0200

    Bug Fix Travis Memory limit

--- bin/ci-scripts/install_test.sh
+++ bin/ci-scripts/install_test.sh
@@ -16,6 +16,9 @@ chmod u+x "${HOME}/bin/phpunit"
 wget https://github.com/satooshi/php-coveralls/releases/download/v1.0.1/coveralls.phar --output-document="${HOME}/bin/coveralls"
 chmod u+x "${HOME}/bin/coveralls"
 
+# Ugly hack
+echo "memory_limit=-1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
+
 # To be removed when this issue will be resolved: https://github.com/composer/composer/issues/5355
 if [ "${COMPOSER_FLAGS}" = '--prefer-lowest' ]; then
     composer update --prefer-dist --no-interaction --prefer-stable --quiet
